fe7db838bb1dcef5c4044125ec690b0c80576764
jusphervergara09 <jusphervergara09@gmail.com>
2024-05-26 04:30:48 +0000

    Working-on-landing-page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryLighter: "#FFFFFF",
+        primary: "#F8FCFF",
+        primaryDarker: "##E4E4E4",
+
+        secondaryLighter: "#0E0E0E",
+        secondary: "#080808",
+        secondaryDarker: "#000000",
+
+        accent: "#0367FD",
+      },
+    },
   },
   plugins: [],
 }
